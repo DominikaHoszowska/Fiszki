6deda2ef224e41378b23eb70ad5017d9792d55c6
Dominika Hoszowska <dominikahoszowska@gmail.com>
2018-11-02 20:07:29 +0100

    create classes

---
+++ Card.h
@@ -0,0 +1,14 @@
+//
+// Created by dominika on 02.11.18.
+//
+
+#ifndef ZPR_CARD_H
+#define ZPR_CARD_H
+
+
+class Card {
+
+};
+
+
+#endif //ZPR_CARD_H
---
+++ Collection.h
@@ -0,0 +1,14 @@
+//
+// Created by dominika on 02.11.18.
+//
+
+#ifndef ZPR_COLLECTION_H
+#define ZPR_COLLECTION_H
+
+
+class Collection {
+
+};
+
+
+#endif //ZPR_COLLECTION_H
